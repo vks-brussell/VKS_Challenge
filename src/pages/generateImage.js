73c6bdcc17aba9528c5388afccb761fe66f70cdf
Julia Dyck <juliaedyck@gmail.com>
2022-08-01 03:55:14 -0400

    comments and cleaning up

--- src/pages/generateImage.js
+++ src/pages/generateImage.js
@@ -1,65 +1,53 @@
-import React, { Component, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
 
-
-
-
-//changed component to arrow function 
+//changed component to arrow function
 
 const ImageGenerator = () => {
- 
   //declaring state
-const [image, setImage] =  useState();
+  const [image, setImage] = useState();
 
- const generateImage = async () => {
+  const generateImage = async () => {
+    //Generate the image
 
+    const { data } = await axios("https://dog.ceo/api/breeds/image/random");
+    //set the image to the data
+    setImage(data.message);
 
-    //Generate the image 
-    
-      const { data } = await axios("https://dog.ceo/api/breeds/image/random");
-      //set the image to the data
-      setImage(data.message);
-  
-    console.log('Generate an image');
+    console.log("Generate an image");
   };
 
-    return (
-      <div className='container'>
-        <CssBaseline />
-        <h2>Click the button to generate a new picture</h2>
-        <Button
-          variant='contained'
-          onClick={generateImage}
-          className='button'
-        >
-          Click Me!
-        </Button>
-    
-      {image ? (
-        // rendering conditionally the image
-        <>
-             <img
-          src={image}
-          alt='Random dog image'
-          id='imgContainer'
-        />
+  return (
+    <div className="container">
+      <CssBaseline />
+      <h2>Click the button to generate a new picture</h2>
+      <Button variant="contained" onClick={generateImage} className="button">
+        Click Me!
+      </Button>
 
+      {image ? (
+        // rendering conditionally the image container
+        <>
+          <img src={image} 
+          alt="Random dog image" 
+          className="container"
+          id="imgContainer" />
         </>
       ) : (
         <>
-        <img
-          src={image}
-          alt='Random dog image'
-          className='img-container hidden'
-          id='imgContainer'
-        />
+          <img
+            src={image}
+            alt="Random dog image"
+            className="img-container hidden"
+            id="imgContainer"
+          />
         </>
       )}
-      </div>
-    );
-  }
-// }
-export default ImageGenerator
+    </div>
+  );
+};
+
+export default ImageGenerator;
--- src/pages/generateImageByBreed.js
+++ src/pages/generateImageByBreed.js
@@ -1,18 +1,18 @@
-import React, { Component, useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
 
-
-//changed component to arrow function 
+//changed component to arrow function
 
 const ImageGenerator = () => {
-// export default class ImageGenerator extends Component {
-
-  const [images, setImages] =  useState();
+// declaring state variables
+  const [images, setImages] = useState();
   const [breeds, setBreeds] = useState();
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+
+// useEffecct to load breeds on page refresh
 
   useEffect(() => {
     getBreed();
@@ -21,87 +21,87 @@ const ImageGenerator = () => {
   //Populate the select's options
 
   const getBreed = async () => {
-    setLoading(true)
+    setLoading(true);
     const { data } = await axios("https://dog.ceo/api/breeds/list/all");
+    
+    //creating array from data
     const array = Object.keys(data.message);
 
-      //set the image to the data
-      setBreeds(array);
-      setLoading(false)
-
-      
-      
-  }
-  
+    //set the image to the data
+    setBreeds(array);
+    setLoading(false);
+  };
 
   const generateImage = async (e) => {
     //Generate the image HERE
-    const id = e.target.value
-    const { data } = await axios(`https://dog.ceo/api/breed/${id}/images`);
-    // tried to use the following code to connect to backend 
-    // const { data } = await axios.get(`/api/breed/${id}/images`)
-      //set the image to the data
-      setImages(data.message);
-      console.log('Generate an image');
 
+    //set id to selected breed
+    const id = e.target.value;
+    // GET images from breed
+    const { data } = await axios(`https://dog.ceo/api/breed/${id}/images`);
+    
+    // tried to use the following code to connect to backend
+    // const { data } = await axios.get(`/api/breed/${id}/images`)
+
+    //set the image to the data
+    setImages(data.message);
+    console.log("Generate an image");
   };
 
-  
-  
   return (
     <>
-  { loading ? ( <> loading </>) :(
-    <>
-
-      <div className='container'>
-        <CssBaseline />
-        <h2>Select a breed to generate an image </h2>
-        <select id='list' onChange={generateImage} className='select'>
-         
-          {breeds?.map((breed) => {
-return (
-<>
-<option value="none" selected disabled hidden>Select a breed</option>
-<option key={breed} value={breed}>{breed}</option>
-</>
-)
-          })}
-        </select>
-        {images?.map((image) => {
-return (
-
-<>
-{ image ? (
-  // rendering conditionally the images
-  <>
-  
-        <img
-          src={image}
-          alt='Random dog image'
-          id='imgContainer'
-        />
+      {loading ? (
+        <> loading </>
+      ) : (
+        <>
+          <div className="container">
+            <CssBaseline />
+            <h2>Select a breed to generate an image </h2>
+            <select id="list" onChange={generateImage} className="select">
+              {breeds?.map((breed) => {
+                return (
+                  <>
+                    <option value="none" selected disabled hidden>
+                      Select a breed
+                    </option>
+                    <option key={breed} value={breed}>
+                      {breed}
+                    </option>
+                  </>
+                );
+              })}
+            </select>
+            {images?.map((image) => {
+              return (
+                <>
+                  {image ? (
+                    // rendering conditionally the images
+                    <>
+                      <img
+                        src={image}
+                        alt="Random dog image"
+                        className="container"
+                        id="imgContainer"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        src={image}
+                        alt="Random dog image"
+                        className="img-container hidden"
+                        id="imgContainer"
+                      />
+                    </>
+                  )}
+                </>
+              );
+            })}
+          </div>
         </>
-
-        ) : (
-          <>
-
-          <img
-          src={image}
-          alt='Random dog image'
-          className='img-container hidden'
-          id='imgContainer'
-        />
-        </>
-
-
-        )}
-
-</>)})}
-
-      </div>
-      </>)}
+      )}
     </>
-    );
-        }
+  );
+};
 
-export default ImageGenerator
+export default ImageGenerator;
